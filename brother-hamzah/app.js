cee7e2d81c90b8b805ab289494e2f8e181cecbfe
smartrove <olaokunolalekan@gmail.com>
2024-03-18 00:05:33 +0100

    vhjijjkjhcs

---
+++ brother-hamzah/app.js
@@ -0,0 +1,37 @@
+//variable and value;
+
+//there are three key words used to declare a variable in js.
+//let, var, const;
+
+let x = 5;
+// let x = 5;
+var y = 5;
+var y = 6;
+
+const z = 5;
+console.log(x, y);
+
+// 1. make use camelcase or snakecase for variable declarations
+
+//camel case
+const hisName = "Shola";
+const countryName = "Nigeria";
+
+//snake case
+const his_name = "Shola";
+const country_name = "Nigeria";
+
+//some other ways of declaring variables
+const _country = "India";
+const $country = "United States";
+
+//don't of declaration
+
+//avoid using keywords
+
+// const class = 'primary one';
+// const var = 'food'
+// const super = 'hello'
+
+//avoid not using camel or snake case where necessary;
+// avoid starting a variable name with capital letter;
