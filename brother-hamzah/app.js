96c72e2b3e3c08d1fb2ea82a59fb2b31599fdfcc
smartrove <olaokunolalekan@gmail.com>
2024-03-23 23:59:38 +0100

    update

--- brother-hamzah/app.js
+++ brother-hamzah/app.js
@@ -9,7 +9,7 @@ var y = 5;
 var y = 6;
 
 const z = 5;
-console.log(x, y);
+// console.log(x, y);
 
 // 1. make use camelcase or snakecase for variable declarations
 
@@ -35,3 +35,68 @@ const $country = "United States";
 
 //avoid not using camel or snake case where necessary;
 // avoid starting a variable name with capital letter;
+
+//data types in javascript
+//string data type
+//number data type
+//boolean data type
+//object data type
+//array data type
+//undefined data type
+//null data type
+//bigInt data type
+
+//string
+//string data type represents any value that has to do with text;
+
+// const firstName = "Shola";
+// const numberString = "2";
+// console.log(typeof firstName, typeof numberString);
+
+//number
+// const m = 5;
+// const n = 10;
+
+// console.log(typeof m, typeof n);
+
+//boolean
+// const isAged = false;
+// const isCapable = true;
+
+// console.log(typeof isAged, typeof isCapable);
+
+//undefined
+// let p;
+// console.log(typeof p);
+
+//null
+// const booking = null;
+// console.log(booking, typeof booking);
+
+//bigInt
+// const bigInteger = 198341818892745867825678678652796;
+// console.log(typeof bigInteger);
+
+//array
+// const arrayOfNumber = [1, 2, 3, 4, 5, 6, 7, "ola", true];
+// console.log(typeof arrayOfNumber);
+
+//object
+// const person = {
+//   name: "John",
+//   age: 16,
+//   dateOfBirth: 12 - 12 - 1970,
+// };
+
+//data type conversion
+// const number = 5;
+// const numberString = String(number);
+// console.log(typeof numberString);
+
+// const firstName = "John";
+// const stringNumber = Number(firstName);
+// console.log(typeof stringNumber);
+
+// const firstName = "John";
+// const stringBoolean = Boolean(firstName);
+// console.log(typeof stringBoolean);
